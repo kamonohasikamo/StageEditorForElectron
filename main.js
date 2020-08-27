da27de6b6e72198cf28b138124db358eb7de1bb6
kamonohasikamo <arrowsuke@yahoo.co.jp>
2020-08-27 20:53:32 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+app.on('ready', () => {
+	mainWindow = new BrowserWindow({ width: 1200, height: 1000 });
+
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+	// Open Chromium Develop tool
+	// mainWindow.webContents.openDevTools();
+
+	mainWindow.on('closed', function () {
+		mainWindow = null;
+	});
+});
