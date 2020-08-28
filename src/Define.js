e04afb90757e4afca86b5b90a9db0bcf7033bfdc
kamonohasikamo <arrowsuke@yahoo.co.jp>
2020-08-29 01:20:19 +0900

    定数クラスの作成

---
+++ src/Define.js
@@ -0,0 +1,14 @@
+//===========================
+// Define class
+//===========================
+
+// ID
+const WALL_ID = 0;
+const NONE_ID = 1;
+const PLAYER_POSITION_ID = 2;
+
+module.exports = {
+	WALL_ID: WALL_ID,
+	NONE_ID: NONE_ID,
+	PLAYER_POSITION_ID: PLAYER_POSITION_ID,
+};
--- src/mainWindowModel.js
+++ src/mainWindowModel.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
+var define = require('./Define');
+
+var outputPath = "output/";
 var outputCSVData = "1,2,3"; // test
 
 function onPressInputButton() {
 	var width = InputForm.inputWidth.value;
 	var height = InputForm.inputHeight.value;
 	console.log("w: " + width + " , h: " + height);
-	//Write CSV file
-	fs.writeFileSync("output/test.csv", outputCSVData);
+	//console.log("Define = " + define.WALL_ID + ", " + define.NONE_ID)
+}
+
+function outputCSVFile() {
+	fs.writeFileSync(outputPath, outputCSVData);
 }
