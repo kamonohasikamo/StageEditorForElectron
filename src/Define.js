cb402828c4a920834ee853d0fc7a8d43a00aa345
kamonohasikamo <arrowsuke@yahoo.co.jp>
2020-09-05 15:10:28 +0900

    マップ表示アルゴリズムの修正とリファクタリング＆表示リセット機能の実装など

--- src/Define.js
+++ src/Define.js
@@ -7,10 +7,12 @@ const WALL_ID = 0;
 const NONE_ID = 1;
 const ROOM_ID = 100;
 const PLAYER_POSITION_ID = 3;
+const OUTPUT_PATH = "output/";
 
 module.exports = {
 	WALL_ID: WALL_ID,
 	NONE_ID: NONE_ID,
 	ROOM_ID: ROOM_ID,
 	PLAYER_POSITION_ID: PLAYER_POSITION_ID,
+	OUTPUT_PATH: OUTPUT_PATH,
 };
